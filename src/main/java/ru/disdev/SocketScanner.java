983fd86444e0828e60e70000e3e83402bfd1ed40
D1slike <lilipp1q@yandex.ru>
2018-03-31 12:53:48 +0300

    init

---
+++ src/main/java/ru/disdev/SocketScanner.java
@@ -0,0 +1,46 @@
+package ru.disdev;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class SocketScanner {
+
+    public static void main(String... args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter {host} {minport} {maxport} > ");
+            String input = scanner.nextLine();
+            String[] split = input.split(" ");
+            if (split.length != 3) {
+                continue;
+            }
+            int minPort;
+            int maxPort;
+            String host;
+            try {
+                host = split[0];
+                if (host.equalsIgnoreCase("exit")) {
+                    break;
+                }
+                minPort = Integer.parseInt(split[1]);
+                maxPort = Integer.parseInt(split[2]);
+            } catch (Exception ex) {
+                continue;
+            }
+
+            IntStream.range(minPort, maxPort + 1)
+                    .parallel()
+                    .forEach(port -> {
+                        try (Socket socket = new Socket()) {
+                            socket.connect(new InetSocketAddress(host, port), 100);
+                            System.out.printf("%s : %d OPENED\n", host, port);
+                        } catch (IOException e) {
+                            System.out.printf("%s : %d CLOSED\n", host, port);
+                        }
+                    });
+        }
+    }
+}
